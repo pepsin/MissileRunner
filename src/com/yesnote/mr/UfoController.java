ef94c328aea0dc651447da737d1f83f4b4713f79
pepsin <pepsin8@gmail.com>
2012-01-04 12:51:53 +0800

    Change the UfoController Variable,not test yet

--- src/com/yesnote/mr/UfoController.java
+++ src/com/yesnote/mr/UfoController.java
@@ -6,8 +6,7 @@ public class UfoController {
 	
 	public void control(UFO ufo, float accelX, float accelY){
 		if (ufo.state != UFO.ROCKET_STATE_HIT) {
-			double x; // Use this to change the x axis's velocity,in case the
-						// velocity in the demand direction is not change
+			
 			if (accelX <= 5 && accelX > 1) {
 				ufo.degree = 30;
 			} else if (accelX > 5 && accelX <= 10) {
@@ -19,9 +18,7 @@ public class UfoController {
 			} else {
 				ufo.degree = 0;
 			}
-			x = Math.sin(6.28 * ufo.degree / 360); // Change the degree into
-														// radian.
-			ufo.velocity.x = -(float) (3 * x * UFO.ROCKET_VELOCITY);
+			ufo.velocity.x = -(float) (accelX * UFO.ROCKET_VELOCITY / 3);
 
 			if (accelY >= -5 && accelY < 5) {
 				ufo.velocity.y = UFO.ROCKET_VELOCITY;
